8e9baa6a9865b63e82ecb318e8691cbe442e79e4
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-11-18 06:31:14 +0600

    fixed issu

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import CustomProgress from "../Customprogress/CustomProgress";
+import { SkillsData } from "@/data/informationData";
 
 export default async function Skill() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/skill`, {
-    cache: "no-store",
-  });
-  if (!res.ok) throw new Error("Failed to fetch Skill data");
-  const skills: [{ title: string; value: number }] = await res.json();
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/skill`, {
+  //   cache: "no-store",
+  // });
+  // if (!res.ok) throw new Error("Failed to fetch Skill data");
+  // const skills: [{ title: string; value: number }] = await res.json();
   // console.log(skills);
   return (
     <div id="skill" className="py-8 lg:py-16 w-[85%] lg:w-[95%] mx-auto">
@@ -16,7 +17,7 @@ export default async function Skill() {
         </span>
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        {skills.map((skill, id) => (
+        {SkillsData.map((skill, id) => (
           <CustomProgress key={id} value={skill.value} title={skill.title} />
         ))}
       </div>
--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -4,6 +4,7 @@ import Lottie from "lottie-react";
 import React, { useEffect, useState } from "react";
 import { PlanFrom } from "./PlanFrom";
 import Link from "next/link";
+import { workMenu } from "@/data/informationData";
 
 interface WorkItem {
   name: string;
@@ -14,42 +15,42 @@ interface WorkItem {
 }
 
 export default function Work() {
-  const [workdata, setWorkData] = useState<WorkItem[]>([]);
-  const [error, setError] = useState<string | null>(null);
+  // const [workdata, setWorkData] = useState<WorkItem[]>([]);
+  // const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/work`, {
-          cache: "no-store",
-        });
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/work`, {
+  //         cache: "no-store",
+  //       });
 
-        if (!res.ok) {
-          throw new Error("Failed to fetch Work data");
-        }
+  //       if (!res.ok) {
+  //         throw new Error("Failed to fetch Work data");
+  //       }
 
-        const data: WorkItem[] = await res.json();
-        setWorkData(data);
-      } catch (err: any) {
-        setError(err.message || "Something went wrong");
-      }
-    };
+  //       const data: WorkItem[] = await res.json();
+  //       setWorkData(data);
+  //     } catch (err: any) {
+  //       setError(err.message || "Something went wrong");
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
-  if (error) {
-    return <div></div>;
-  }
+  // if (error) {
+  //   return <div></div>;
+  // }
 
-  if (!workdata.length) {
-    return <div></div>;
-  }
+  // if (!workdata.length) {
+  //   return <div></div>;
+  // }
 
   return (
     <div className="bg-primary-1 border-y border-blue-300">
       <div className="grid w-[95%] py-10 mx-auto grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-7">
-        {workdata.map((work, id) =>
+        {workMenu.map((work, id) =>
           work.plane ? (
             <PlanFrom work={work} key={id} />
           ) : (
